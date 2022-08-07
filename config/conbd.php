b7c304e39c011977d0f19884b428d08da5a92cab
stalinarmijo84 <stalinarmijo84@hotmail.com>
2022-08-06 21:52:57 -0500

    secuenciales

--- config/conbd.php
+++ config/conbd.php
@@ -4,7 +4,7 @@ class Conexion{
         $database_username = 'desarrollo';
         $database_password = 'armijo123.';
         $dbname="vecoin_bd";
-        $dsn = 'mysql:host=88.99.30.217;dbname=' . $dbname;
+        $dsn = 'mysql:host=88.99.30.217:3306;dbname=' . $dbname;
         $conexion = null;   
         try{
             $conexion = new PDO($dsn, $database_username, $database_password ); 

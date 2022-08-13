baacd92ad356e49a633101c5534840223519f81d
Norman Nuñez <normanmos@hotmail.com>
2022-08-13 15:25:20 -0500

    report

--- views/reportes/stock_productos.php
+++ views/reportes/stock_productos.php
@@ -13,11 +13,11 @@ class PDF extends FPDF
         $sol_cred = $rep->ReporteStockProductos();
         if ($sol_cred) {
             foreach ($sol_cred as $scred) {
-                $_SESSION["dir"] = $scred["direccion"];
-                $_SESSION["tel"] = $scred["telefono"];
+                //$_SESSION["dir"] = $scred["direccion"];
+                //$_SESSION["tel"] = $scred["telefono"];
                 $title = 'STOCK DE PRODUCTOS A LA FECHA';
-                $ta = 'Responsable: ';
-                $fh = 'Fecha/Hora: ';
+                //$ta = 'Responsable: ';
+                //$fh = 'Fecha/Hora: ';
                 $this->SetFont('Arial', 'B', 10);
                 date_default_timezone_set('America/Guayaquil');
                 $DateAndTime = date('d/m/Y h:i:s a', time());
@@ -36,8 +36,8 @@ class PDF extends FPDF
                 $this->SetFont('Arial', 'I', 7);
                 $this->Cell(190, 5, utf8_decode($fi) . $DateAndTime, 0, 1, 'R', 0);
                 $this->SetFont('Arial', 'I', 8);
-                $FechaCred = date('d/m/Y', strtotime($scred["fecha"]));
-                $this->Cell(30, 5, utf8_decode($fh . $FechaCred . '  -  ' . $ta . $scred["responsable"]), 0, 1, '');
+                //$FechaCred = date('d/m/Y', strtotime($scred["fecha"]));
+                //$this->Cell(30, 5, utf8_decode($fh . $FechaCred . '  -  ' . $ta . $scred["responsable"]), 0, 1, '');
             }
         }
     }
@@ -65,34 +65,43 @@ $pdf->AddPage();
 $rep = new ReporteModel();
 $pdf->SetFillColor(150, 150, 150);
 $pdf->SetTextColor(3, 3, 3);
-$IdSecuencial = (isset($_REQUEST['IdSecuencial'])) ? $_REQUEST['IdSecuencial'] : '';
-$resultados = $rep->ReporteDetOrdenEntrada($IdSecuencial);
+$resultados = $rep->ReporteStockProductos();
+
 if ($resultados) {
     $sum = 0;
     $pdf->SetFont('Arial', 'B', 10);
-    $pdf->Cell(190, 5, 'DETALLE DE PRODUCTOS', 1, 1, 'C', true);
+    $pdf->Cell(190, 5, 'CANTIDADES DISPONIBLES A LA FECHA DE IMPRESION', 1, 1, 'C', true);
     $pdf->SetFont('Arial', 'B', 8);
+    $pdf->Cell(20, 5, utf8_decode('Codigo'), 1, 0, 'C', false);
+    $pdf->Cell(20, 5, utf8_decode('Empresa'), 1, 0, 'C', false);
+    $pdf->Cell(25, 5, utf8_decode('Producto'), 1, 0, 'C', false);
+    $pdf->Cell(50, 5, utf8_decode('Proveedor'), 1, 0, 'C', false);
+    $pdf->Cell(50, 5, utf8_decode('Bodega'), 1, 0, 'C', false);
+    $pdf->Cell(20, 5, utf8_decode('U.Medida'), 1, 0, 'C', false);
     $pdf->Cell(20, 5, utf8_decode('Cantidad'), 1, 0, 'C', false);
-    $pdf->Cell(25, 5, utf8_decode('U.Medida'), 1, 0, 'C', false);
-    $pdf->Cell(95, 5, utf8_decode('Producto'), 1, 0, 'C', false);
     $pdf->Cell(20, 5, utf8_decode('Precio'), 1, 0, 'C', false);
-    $pdf->Cell(30, 5, utf8_decode('Monto'), 1, 1, 'C', false);
+    $pdf->Cell(20, 5, utf8_decode('Valorización'), 1, 0, 'C', false);
+    $pdf->Cell(30, 5, utf8_decode('Estado'), 1, 1, 'C', false);
     foreach ($resultados as $re) {
         $pdf->SetFont('Arial', 'I', 8);
+        $pdf->Cell(20, 5, utf8_decode($re["id_producto"]), 1, 0, 'C', false);
+        $pdf->Cell(20, 5, utf8_decode($re["compania"]), 1, 0, 'C', false);
+        $pdf->Cell(25, 5, utf8_decode($re["nombre_producto"]), 1, 0, 'C', false);
+        $pdf->Cell(50, 5, utf8_decode($re["proveedor"]), 1, 0, 'C', false);
+        $pdf->Cell(50, 5, utf8_decode($re["bodega"]), 1, 0, 'C', false);
+        $pdf->Cell(20, 5, utf8_decode($re["umedida"]), 1, 0, 'C', false);
         $pdf->Cell(20, 5, utf8_decode($re["cantidad"]), 1, 0, 'C', false);
-        $pdf->Cell(25, 5, utf8_decode($re["umedida"]), 1, 0, 'C', false);
-        $pdf->Cell(95, 5, utf8_decode($re["producto"]), 1, 0, 'L', false);
         $pdf->Cell(20, 5, utf8_decode($re["precio"]), 1, 0, 'C', false);
-        $pdf->Cell(30, 5, utf8_decode(number_format($re["monto"], 2, ".", ",")), 1, 1, 'C', false);
-        $sum += $re["cantidad"] * $re["precio"];
+        $pdf->Cell(20, 5, utf8_decode($re["valorizacion"]), 1, 0, 'C', false);
+        $pdf->Cell(30, 5, utf8_decode($re["id_estado"]), 1, 1, 'C', false);
     }
     $pdf->SetFont('Arial', 'B', 10);
     $pdf->Cell(160, 5, 'TOTAL:', 1, 0, 'R', true);
     $pdf->SetFont('Arial', 'B', 9);
-    $pdf->Cell(30, 5, number_format($sum, 2, ".", ","), 1, 1, 'C', false);
+    //$pdf->Cell(30, 5, number_format($sum, 2, ".", ","), 1, 1, 'C', false);
 }
-$IdSecuencial = (isset($_REQUEST['IdSecuencial'])) ? $_REQUEST['IdSecuencial'] : '';
-$sol_cred = $rep->ReporteCabOrdenEntrada($IdSecuencial);
+//$IdSecuencial = (isset($_REQUEST['IdSecuencial'])) ? $_REQUEST['IdSecuencial'] : '';
+$sol_cred = $rep->ReporteStockProductos();
 $pdf->SetFont('Arial', 'I', 10);
 if ($sol_cred) {
     foreach ($sol_cred as $sc) {
